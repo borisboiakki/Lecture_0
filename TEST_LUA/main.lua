cbbc768cca545656a60baebf58ba16cc9fecb5c0
simone bergamo <borisjackboiakki@hotmail.it>
2018-08-26 13:15:20 +0200

    Version 2

--- TEST_LUA/main.lua
+++ TEST_LUA/main.lua
@@ -1,29 +1,44 @@
 
+push = require 'push'
+
 WINDOWS_WIDTH = 1280
 WINDOWS_HEIGHT = 720
 
+VIRTUAL_WIDTH = 432
+VIRTUAL_HEIGHT = 243
+
+
 function love.load()
-    love.window.setMode(WINDOWS_WIDTH, WINDOWS_HEIGHT, {
+    love.graphics.setDefaultFilter('nearest','nearest')
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOWS_WIDTH, WINDOWS_HEIGHT, {
         fullscreen = false,
         resizable = false,
         vsync = true
     })
-    love.graphics.setDefaultFilter()
     
 end
 
-function love.update(dt)
-
+function love.keypressed(key)
+    if key == 'escape' then
+        love.event.quit()
+    end
 end
+
+--function love.update(dt)
+--end
 
 
 function love.draw()
+    push:apply('start')
+
     love.graphics.printf(
         "Hello Pong",
         0,
-        WINDOWS_HEIGHT / 2 - 6,
-        WINDOWS_WIDTH,
+        VIRTUAL_HEIGHT / 2 - 6,
+        VIRTUAL_WIDTH,
         'center')
+
+    push:apply('end')
 end
 
 
