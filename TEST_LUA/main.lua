8857d2091b6275b095c272ab4a8af2919df2654b
simone bergamo <borisjackboiakki@hotmail.it>
2018-08-26 13:32:41 +0200

    Up to Pong2

--- TEST_LUA/main.lua
+++ TEST_LUA/main.lua
@@ -10,6 +10,11 @@ VIRTUAL_HEIGHT = 243
 
 function love.load()
     love.graphics.setDefaultFilter('nearest','nearest')
+
+    smallFont = love.graphics.newFont('font.ttf',8)
+
+    love.graphics.setFont(smallFont)
+
     push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOWS_WIDTH, WINDOWS_HEIGHT, {
         fullscreen = false,
         resizable = false,
@@ -31,13 +36,25 @@ end
 function love.draw()
     push:apply('start')
 
+    love.graphics.clear(0, 0, 0, 1)
+
+
     love.graphics.printf(
         "Hello Pong",
         0,
-        VIRTUAL_HEIGHT / 2 - 6,
+        10,
         VIRTUAL_WIDTH,
         'center')
 
+    love.graphics.rectangle('fill', 10 , 30, 5, 20)
+
+    love.graphics.rectangle(
+        'fill',
+        VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 50,
+         5,20)
+
+    love.graphics.rectangle('fill',VIRTUAL_WIDTH/2 -2, VIRTUAL_HEIGHT /2 -2, 4,4)
+    
     push:apply('end')
 end
 
